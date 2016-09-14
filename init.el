7bd57fc11a3da066a4fc8194642a554d10415c61
yeonghoey <yeonghoey@gmail.com>
2016-09-14 11:50:30 +0900

    Refine init.el

--- init.el
+++ init.el
@@ -1,22 +1,18 @@
-;; package settings
 (require 'package)
-
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-milkbox" . "http://melpa.milkbox.net/packages/"))
 
-
-;; package init
 (defun require-package (package)
   (when (not (package-installed-p package))
     (package-install package)))
 
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-(setq package-enable-at-startup nil)
 (package-initialize)
 
+(when (not (file-exists-p package-user-dir))
+  (package-refresh-contents))
+
 (require-package 'projectile)
+
 (projectile-global-mode)
